8525a40e54c2ec970820c7fcb47a811b73a68479
TizianaAndreaOrlando <81337862+TizianaAndreaOrlando@users.noreply.github.com>
2022-01-07 15:22:49 -0300

    Add files via upload

--- BTC_convertor.py
+++ BTC_convertor.py
@@ -1,26 +1,26 @@
-#Definimos la función "Calculadora".Todos los datos estan en USD exceptuando por la salida final representada en BTC
-def calculator(C_0,BTC_price):
-    value1 = str(C_0/BTC_price)         #Regla de 3; nos da el valor en moneda por capital inicial invertido (C_0)
-    value1 = value1 + " " + moneda
-    print(f"\nUsted tiene: {value1}")
+#Definimos la función "Calculadora".Todos los datos estan en USD exceptuando por la salida final representada en la moneda elegida.
+def calculator(C_0,coin_price):
+    value1 = (C_0/coin_price)         #Regla de 3; nos da el valor en moneda por capital inicial invertido (C_0)
+    value1 = round(value1,6)
+    print(f"\nUsted tiene: {value1} {coin}")
     return value1       
 
 #************* ***************** **************** *********************
 # Ingresamos los datos iniciales de la operación y aplicamos los calculos escenciales.
 
-moneda = str(input("Ingrese el tipo de moneda: "))
+coin = str(input("Ingrese el tipo de moneda: "))
 C_0 = float(input("Ingrese su capital inicial en USD: "))
-BTC_price= float(input("Ingrese el precio de BTCxUSD ")) #Trabajar en API's para no tenerlo que hacer manual. 
-if BTC_price == 0:
+coin_price= float(input(f"Ingrese el precio de {coin}xUSD ")) #Trabajar en API's para no tenerlo que hacer manual. 
+if coin_price == 0:
     exit          #En el caso de que nos de un valor = 0, el programa se cierra.
 else:
-    calculator(C_0,BTC_price)
-value = (C_0/BTC_price)        #La defino como variable global y así poder usarla en otras funciones
+    calculator(C_0,coin_price)
+value = (C_0/coin_price)        #La defino como variable global y así poder usarla en otras funciones
 
-summary = {"moneda": moneda,   #Creamos un diccionario para poder resumir al final del codigo nuestros datos. 
+summary = {"moneda": coin,   #Creamos un diccionario para poder resumir al final del codigo nuestros datos. 
           "Monto inicial": C_0,
-          "Precio BTC inicial": BTC_price,
-          "Cant. BTC en posesión" : value,
+          "Precio moneda inicial": coin_price,
+          "Cant. moneda en posesión" : value,
           }  
 
 #************* ***************** **************** ********************* 
@@ -39,7 +39,7 @@ def profitableness():
     convertor = (C_f/value)
     convertor = round(convertor,2)
     print(f"\nUsted espera sacar {absolut_value}$ USD de la transacción y su capital final será de: {C_f}$ USD. " 
-          f"\nEl valor de BTC debería llegar a {convertor}$ para obtener una rentabilidad del {i*100} % de los {C_0}$ invertidos.")
+          f"\nEl valor de {coin} debería llegar a {convertor}$ para obtener una rentabilidad del {i*100} % de los {C_0}$ invertidos.")
     
     print("\n************* **************** CONVERSIÓN PESO-ARGENTINO **************** *********************\n") 
     print("Esta parte del programa nos permitira convertir nuestros dolares en pesos Argentinos.\n")
@@ -57,7 +57,6 @@ profitableness()
 
 print("Datos finales:\n ",summary)
 
+input("\nPresione cualquier tecla para finalizar.")
 
 #************* ***************** **************** *********************
-
-    
